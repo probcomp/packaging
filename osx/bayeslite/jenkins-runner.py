7e15f94f624fabb2b246b317cfc9684a1aa56cc5
Gregory Marton <gremio@acm.org>
2015-10-08 23:01:15 -0400

    missed one instance of the build_utils to shell_utils rename

--- osx/bayeslite/jenkins-runner.py
+++ osx/bayeslite/jenkins-runner.py
@@ -53,7 +53,7 @@ def test_run(cmd):
   run("ssh test@%s %s" % (HOST, shellquote(cmd)))
 
 def build_dmg():
-  run("scp build_dmg.py build_utils.py build@%s:" % (HOST,))
+  run("scp build_dmg.py shell_utils.py build@%s:" % (HOST,))
   build_run('PATH="%s:\\$PATH" python build_dmg.py' % (HPATH,))
   run("scp build@%s:Desktop/Bayeslite*.dmg %s" % (HOST, SCRATCH))
   name = build_outputof("cd Desktop && ls -t Bayeslite*.dmg | tail -1").strip()
