a153c0dda4ad79fef92ff75f9f123bd7f4ed322c
Richard Tibbetts <tibbetts@innocuous.org>
2015-10-28 10:51:07 -0400

    Better run script but still doesn't work on probcomp-2

--- docker/bayeslite/run.sh
+++ docker/bayeslite/run.sh
@@ -3,4 +3,5 @@
 set -ex
 
 docker build -t bayeslite .
-docker run -p 8888 -t bayeslite
+# Delete it when it exists, keep it on a terminal and attached.
+docker run --rm -i -t -p 8888:8888 bayeslite
