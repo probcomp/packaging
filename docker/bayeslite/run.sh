e00bb17253fba7b24be1a44e849196c0c5d86af1
Gregory Marton <gremio@acm.org>
2016-02-08 22:34:58 -0500

    Successfully builds and runs an up-to-date notebook server

--- docker/bayeslite/run.sh
+++ docker/bayeslite/run.sh
@@ -3,5 +3,15 @@
 set -ex
 
 docker build -t bayeslite .
+notebook_url="http://`docker-machine ip default`:8888/"
+if [ -n "$BROWSER" ]; then
+    $BROWSER "$notebook_url"
+elif [ "Darwin" == "`uname -s`" ]; then
+    open "$notebook_url"
+elif which python > /dev/null; then
+    python -mwebbrowser "$notebook_url"
+else
+    echo "The notebook will be available at $notebook_url"
+fi
 # Delete it when it exists, keep it on a terminal and attached.
-docker run --rm -i -t -p 8888:8888 bayeslite
+docker run --rm -i -t --net=host bayeslite
